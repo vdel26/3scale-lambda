0370e78e013c8989b77885321df988f212d83f77
Victor Delgado <vdel26@gmail.com>
2015-08-13 01:01:21 +0200

    accept destination path as a parameter

--- bin/main.js
+++ bin/main.js
@@ -1,3 +1,3 @@
 var generator = require('../index');
 
-generator('TESTKEY');
+generator('TESTKEY', '/Users/victordg/Code/3scale/3scale-lambda-auth/example/');
--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 var archiver = require('archiver');
-var p        = require('path');
+var path     = require('path');
 var fs       = require('fs');
 var split    = require('split');
 var through  = require('through');
 
-var output = fs.createWriteStream(__dirname + '/3scale-lambda-auth.zip');
+// set up archiver
+
 var archive = archiver('zip');
-
-output.on('close', function () {
-  console.log(archive.pointer() + ' total bytes');
-  console.log('zip bundle is ready');
-});
-
 archive.on('error', function (err) {
   throw err;
 });
 
-// configure destination of zipped bundle
-
-archive.pipe(output);
-
 // get a readable-writable stream that applies a
-// string replacement to an input stream
+// string replacement over an input stream
 
 function getReplacementStream (providerKey) {
   return through(function (line) {
@@ -38,9 +29,24 @@ function getInputStream (providerKey) {
           .pipe(getReplacementStream(providerKey));
 }
 
-// public API
+/**
+ * Generates a Lambda function to authenticate API calls with 3scale.
+ * The output is a zip bundle including the Lambda function and all its
+ * dependencies.
+ * @param  {String} providerKey     – a 3scale provider key
+ * @param  {String} destinationPath – an absolute path (excluding filename)
+ */
 
-module.exports = function generator (providerKey) {
+module.exports = function generator (providerKey, destinationPath) {
+  var output = fs.createWriteStream(
+    path.join(destinationPath, '3scale-lambda-auth.zip')
+  );
+  output.on('close', function () {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('zip bundle is ready');
+  });
+  archive.pipe(output);
+
   archive
     .append(getInputStream(providerKey), { name: 'aws_3scale_auth.js' })
     .directory('input/node_modules', 'node_modules')
