a1d3e1222e81bbc7c947edb418d1b57f0d8afa7b
Victor Delgado <vdel26@gmail.com>
2015-08-13 15:03:38 +0200

    report to method names based on requested path

--- input/aws_3scale_auth.js
+++ input/aws_3scale_auth.js
@@ -1,20 +1,45 @@
 var threescale = require('3scale');
 var Client = threescale.Client;
-var client = new Client("PROVIDER_KEY");
+var client = new Client('PROVIDER_KEY');
 
-var authenticate = function (user_key, callback) {
-  client.authrep_with_user_key({"user_key": user_key, "usage": { "hits": 1 } }, function (resp) {
-    if (resp.is_success()) {
-      callback(null, resp.is_success());
+function getMethodName (path, method) {
+  if (!path || !method) return 'hits';
+  var pathFrag = path.replace(/^\/|\/$/g, '').replace(/\//g, '_');
+  return pathFrag + '_' + method;
+}
+
+function authenticate (user_key, method, callback) {
+  var options = { 'user_key': user_key, 'usage': {} };
+  options['usage'][method] = 1;
+
+  client.authrep_with_user_key(options, function (res) {
+    if (res.is_success()) {
+      callback(null, res.is_success());
     } else {
-      callback("403, unauthorized");
+      callback('403, unauthorized');
     }
   });
 };
 
+/**
+ * Lambda function to be used as an AWS API gateway endpoint handler.
+ * Requires setting up the following Mapping Template:
+ *   - Content-Type: application/json
+ *   - Template:
+ *       {
+ *         "user_key": "$input.params('user_key')",
+ *         "resourcePath": "$context.resourcePath",
+ *         "httpMethod": "$context.httpMethod",
+ *         "body": $input.json('$')
+ *       }
+ *
+ */
+
 exports.handler = function(event, context) {
+  var method = getMethodName(event.resourcePath, event.httpMethod);
+
   if (event.user_key) {
-    authenticate(event.user_key, function (err,res) {
+    authenticate(event.user_key, method, function (err, res) {
       if (err) {
         context.fail(err);
       } else {
@@ -22,7 +47,6 @@ exports.handler = function(event, context) {
       }
     });
   } else {
-    context.fail("user_key missing")
+    context.fail('user_key missing');
   }
 };
-
